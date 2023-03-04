d45a2109813c5402ce28b3cc5975a24dafe0c0d6
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-03-04 15:27:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from robyn import Robyn, jsonify
 from controllers import all_books, new_book, book_by_id, delete_book, update_book
 import json
-from schemas import Book
+
 
 app = Robyn(__file__)
 
@@ -11,21 +11,17 @@ async def create_book(request):
     body = bytearray(request['body']).decode("utf-8")
     json_body = json.loads(body)
 
-    title = json_body['title']
-    author = json_body['author']
-    pages_num = json_body['pages_num']
-    review = json_body['review']
-
-    book = new_book(title, author, pages_num, review)
-    
-    return book
+    try:
+        book = new_book(json_body['title'], json_body['author'])
+        return {"status_code":201, "body": book, "type": "json"}
+    except:
+        return {"status_code":500, "body": "Internal Server Error", "type": "text"}
 
 
 @app.get("/books")
 async def books():
     books = all_books()
-    return books
-
+    return {"status_code":200, "body": books, "type": "json"}
 
 
 @app.get("/book/:id")
@@ -33,13 +29,15 @@ async def get_book(request):
     id = request['params']['id']
 
     book = book_by_id(id)
-    
 
-    if book == None:
-        return {"status_code":404, "body": "Book not Found", "type": "text"}
-    else:
-        return book
-    
+    try:
+        if book == None:
+            return {"status_code":404, "body": "Book not Found", "type": "text"}
+        else:
+            return {"status_code":200, "body": book, "type": "json"}
+    except:
+         return {"status_code":500, "body": "Internal Server Error", "type": "text"}    
+        
         
 @app.put("/book/:id")
 async def update(request):
@@ -50,30 +48,35 @@ async def update(request):
 
     title = json_body['title']
     author = json_body['author']
-    pages_num = json_body['pages_num']
-    review = json_body['review']
 
     book_id = book_by_id(id)
 
-    if book_id == []:
+    if book_id == None:
         return {"status_code":404, "body": "Book not Found", "type": "text"}
-    else:    
-        book = update_book(title, author, pages_num, review, id)
-        print(book)
-        return book
+    else:
+        try: 
+            book = update_book(title, author, id)
+            return {"status_code":200, "body": book, "type": "json"}
+        except:
+            return {"status_code":500, "body": "Internal Server Error", "type": "text"}
     
 
 @app.delete("/book/:id")
 async def delete(request):
     id = request['params']['id']
 
-    book = delete_book(id)
+    book_id = book_by_id(id)
 
-    if book != "Book deleted":
+    if book_id == None:
         return {"status_code":404, "body": "Book not Found", "type": "text"}
-    else:    
-        return book    
+    else:
+        try: 
+            delete_book(id)
+            return {"status_code":200, "body": "Book deleted", "type": "json"}
+        except:
+            return {"status_code":500, "body": "Internal Server Error", "type": "text"}  
 
+    
 
 
 app.start(port=8000, url="0.0.0.0")
